8223b086e4ceac65df9bcbb97a21b84e53510472
Ahmad Zaki <zaki.bm@gmail.com>
2016-08-31 08:28:46 +0800

    Update start.sh for RisingHF Gateway
    The gw reset procedure is a little different from imst board.

--- start.sh
+++ start.sh
@@ -1,16 +1,31 @@
 #! /bin/bash
 
-# Reset iC880a PIN
-SX1301_RESET_BCM_PIN=25
-echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export 
-echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction 
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
-sleep 0.1  
-echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value 
-sleep 0.1  
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
+# For RisingHF Gateway
+# This script is intended to be used to reset RHF0M301 module
+# SX1301 chip need to be mannually reset before working
+
+RHF0M301_RESET_PIN=7
+WAIT_TIME=0.1
+
+if [ $(id -u) -ne "0" ]
+then
+    echo "ERROR: Please run as root"
+    exit 1
+fi
+
+if [ ! -d /sys/class/gpio/gpio$RHF0M301_RESET_PIN ]
+then
+    echo "$RHF0M301_RESET_PIN" > /sys/class/gpio/export
+    sleep $WAIT_TIME
+fi
+echo "out" > /sys/class/gpio/gpio$RHF0M301_RESET_PIN/direction
+sleep $WAIT_TIME
+echo "1" > /sys/class/gpio/gpio$RHF0M301_RESET_PIN/value
+sleep $WAIT_TIME
+echo "0" > /sys/class/gpio/gpio$RHF0M301_RESET_PIN/value
+
+echo "Gateway reset successfully"
+
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
